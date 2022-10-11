a75101825078d4062cb2e37921302afcc2268108
Yagnik <59044382+Yagnik-Gohil@users.noreply.github.com>
2022-10-11 14:35:39 +0530

    Updated Booking controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,7 +60,6 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100;
-
   await Booking.create({ tour, user, price });
 };
 
